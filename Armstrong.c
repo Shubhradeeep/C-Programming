5d82c35b4afc8447a025577185161c5af8fe66c2
Shubhradeep Maity <shubhradeepmaity3@gmail.com>
2022-09-27 09:46:27 +0530

    initial commit

---
+++ Armstrong.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include<math.h>
+int main()
+{
+    int n1, n2, temp, d, sum,count, i;
+    printf("Enter the lower limit and upper limit: ");
+    scanf("%d %d", &n1, &n2);
+    for (i = n1; i <= n2; i++)
+    {
+        temp = i;
+        sum=0;
+        count=0;
+        while(temp!=0 )
+        {
+            temp=temp/10;
+            count++;
+        }
+        temp = i;
+        while (temp != 0)
+        {
+
+            d = temp % 10;
+            temp = temp / 10;
+            sum = sum + ((int)pow(d,count));
+        }
+        if (sum == i)
+            printf("%d\t", i);
+    }
+    return 0;
+}
